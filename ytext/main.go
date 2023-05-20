b2226cc29b667e338e1c6e70bcd3b9ef237a2c11
ugjka <ugjka@proton.me>
2023-05-20 17:08:49 +0300

    better shit

--- ytext/main.go
+++ ytext/main.go
@@ -25,7 +25,7 @@
 //
 // *******************
 // About:
-// Get a transcript of a youtube video by ID
+// Get transcriptions of youtube videos by IDs or URLs
 //
 // Needs yt-dlp
 // Tested only on Linux
@@ -44,13 +44,15 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path"
 	"strings"
+	"syscall"
 )
 
-const USAGE = `Usage: %s [OPTIONS] [Youtube ID]
+const USAGE = `Usage: %s [OPTIONS] [Youtube IDs or URLs]
 
-Get a transcript of a youtube video by ID
+Get transcriptions of youtube videos by IDs or URLs
 (default language: 'en')
 
 Options:
@@ -58,7 +60,7 @@ Options:
 	  language iso code, 
 `
 
-const YTCMD = "yt-dlp -4 --skip-download --write-auto-subs --sub-format json3 --sub-langs %s --"
+const YTCMD = "yt-dlp -4 -i --skip-download --write-auto-subs --sub-format json3 --sub-langs %s --"
 
 func main() {
 	// check for yt-dlp
@@ -67,70 +69,100 @@ func main() {
 		os.Exit(1)
 	}
 
-	lang := flag.String("lang", "en", "language (iso code)")
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, USAGE, path.Base(os.Args[0]))
-	}
-	flag.Parse()
-	if len(flag.Args()) == 0 {
-		fmt.Fprintln(os.Stderr, "error: no youtube id given")
+	exe := path.Base(os.Args[0])
+
+	// trap ctrl+c and kill
+	cmd := &exec.Cmd{}
+	tmp, err := os.MkdirTemp("", exe)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	id := flag.Args()[0]
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-sig
+		if cmd.Process != nil {
+			cmd.Process.Kill()
+		}
+		os.RemoveAll(tmp)
+		fmt.Fprintf(os.Stderr, "error: %s aborted\n", exe)
+		os.Exit(1)
+	}()
+
+	lang := flag.String("lang", "en", "language (iso code)")
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, USAGE, exe)
+	}
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		fmt.Fprintln(os.Stderr, "error: no youtube IDs or URLs given")
+		os.Exit(1)
+	}
+	ids := flag.Args()
 
 	curdir, err := os.Getwd()
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
 	cmdarr := strings.Split(fmt.Sprintf(YTCMD, *lang), " ")
-	tmp, err := os.MkdirTemp("", "ytext")
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+
 	err = os.Chdir(tmp)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	cmdarr = append(cmdarr, id)
-	cmd := exec.Command(cmdarr[0], cmdarr[1:]...)
+
+	cmdarr = append(cmdarr, ids...)
+	cmd = exec.Command(cmdarr[0], cmdarr[1:]...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
+
 	err = cmd.Run()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.RemoveAll(tmp)
-		os.Exit(1)
+		fmt.Fprintln(os.Stderr, "error: there were some errors")
 	}
+
 	dir, err := os.ReadDir("./")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+
 	if len(dir) == 0 {
-		fmt.Fprintln(os.Stderr, "error: transcript not found")
+		fmt.Fprintln(os.Stderr, "error: transcriptions not found")
 		os.RemoveAll(tmp)
 		os.Exit(1)
 	}
-	name := dir[0].Name()
-	jsonfile, err := os.Open(name)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.RemoveAll(tmp)
-		os.Exit(1)
+
+	for _, file := range dir {
+		name := file.Name()
+		jsonfile, err := os.Open(name)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.RemoveAll(tmp)
+			os.Exit(1)
+		}
+		err = os.Chdir(curdir)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		err = os.WriteFile(name[:len(name)-(4+len(*lang))]+".txt", json3toText(jsonfile), 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		err = os.Chdir(tmp)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
-	err = os.Chdir(curdir)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-	err = os.WriteFile(name[:len(name)-6]+".txt", json3toText(jsonfile), 0644)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+
 	err = os.RemoveAll(tmp)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
